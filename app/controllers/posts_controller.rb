5b50698b299bf68bb4e392f00843738c6042bf04
Alexandre G.-Raymond <alex@ndre.gr>
2013-08-03 09:05:36 +0200

    Limit post CRUD to authenticated users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /

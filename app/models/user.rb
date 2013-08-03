91a5a9e8a4f8633af219f32c0c342b9efa8c5292
Alexandre G.-Raymond <alex@ndre.gr>
2013-08-03 08:37:39 +0200

    Add User.is_admin attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable, :confirmable, :registerable
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728104546) do
+ActiveRecord::Schema.define(version: 20130803061919) do
 
   create_table "posts", force: true do |t|
     t.datetime "created_at"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20130728104546) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

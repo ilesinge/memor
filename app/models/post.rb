2d70f1424cce72c2fde2b61f957b5f57e3aefa4e
Alexandre G.-Raymond <alex@ndre.gr>
2013-08-24 10:09:36 +0200

    Validate uniqueness of post url

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
   validates :title, presence: true
-  validates :url, presence: true, url: true
+  validates :url, presence: true, url: true, uniqueness: true
   validates :user, presence: true
 end

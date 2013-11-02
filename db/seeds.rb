375fe215f2ba2cee0c1f88b491797480f0728fc3
Alexandre G.-Raymond <alex@ndre.gr>
2013-11-02 18:44:58 +0100

    Update admin seed (use username)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User.find_or_create_by_email('admin@example.com')
+admin = User.find_or_create_by_username('administrator')
 admin.password = 'memorFTW!'
 admin.is_admin = true
 admin.save!
